bb5d3b9a27721d88b42d04451c5965eeddf33098
lro99 <164283363+lro99@users.noreply.github.com>
2025-05-15 11:56:44 -0600

    Update predict.py
    load pipeline, perform predictions on test set, return auc

--- src/predict.py
+++ src/predict.py
@@ -1,18 +1,27 @@
-# predict.py
 from pyspark.ml.classification import LogisticRegressionModel
-from preprocess import preprocess_data
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.ml import PipelineModel
+# from preprocess import preprocess_data
+from pyspark.sql import SparkSession
+
+# model path -will edit to make nonlocal
+model_path = '/content/drive/MyDrive/Documents/Data Career/fraud_classification/fraud_class_pyspark'
 
 def predict(data_path, model_path):
-    df = preprocess_data(data_path)
-    _, test_df = df.randomSplit([0.8, 0.2], seed=42)
+  spark = SparkSession.builder.appName("FraudDetection").config("spark.hadoop.fs.s3a.aws.credentials.provider", "com.amazonaws.auth.DefaultAWSCredentialsProviderChain").config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem").config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.11.1026").getOrCreate()
 
-    model = LogisticRegressionModel.load(model_path)
-    predictions = model.transform(test_df)
-    predictions.select("features", "prediction", "probability").show(10)
+  # loading data, splitting
+  df = spark.read.parquet(data_path)
+  _, df = df.randomSplit([0.8, 0.2], seed=42)
 
-if __name__ == "__main__":
-    data_path = [
-        "s3a://fraudclassificationdata.s3.us-west-1.amazonaws.com/train-00000-of-00002.parquet",
-        "s3a://fraudclassificationdata.s3.us-west-1.amazonaws.com/train-00001-of-00002.parquet"
-    ]
-    predict(data_path, "model/fraud_pipeline")
+  # no need to preprocess, already included in pipeline
+
+  # load saved model to predict
+  pipeline_model = PipelineModel.load(model_path)
+  predictions = pipeline_model.transform(df)
+
+  evaluator = BinaryClassificationEvaluator(rawPredictionCol='rawPrediction', labelCol='is_fraud')
+  eval = evaluator.evaluate(predictions)
+
+  # auc
+  return eval
