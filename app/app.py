0631f1f0a9ffe255edb5560425453f5bdffe4db9
lro99 <164283363+lro99@users.noreply.github.com>
2025-05-18 16:42:05 -0600

    edited spark session in app

--- app/app.py
+++ app/app.py
@@ -3,8 +3,13 @@ from pyspark.sql import SparkSession
 from pyspark.ml import PipelineModel
 
 app = Flask(__name__)
-spark = SparkSession.builder.appName('FraudDetectionAPI').getOrCreate()
-model = PipelineModel.load('/app/model/fraud_pipeline')
+spark = SparkSession.builder \
+    .appName("FraudDetection") \
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+    .config("spark.hadoop.fs.s3a.aws.credentials.provider", "com.amazonaws.auth.DefaultAWSCredentialsProviderChain") \
+    .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.2,com.amazonaws:aws-java-sdk-bundle:1.11.901") \
+    .getOrCreate()
+model = PipelineModel.load('/app/model')
 
 @app.route('/predict', methods=['POST'])
 def predict():
