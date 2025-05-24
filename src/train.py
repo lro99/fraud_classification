16a3e9ca2b4a97c29d3e3deb4bf2f55fc95be9e4
lro99 <164283363+lro99@users.noreply.github.com>
2025-05-24 17:19:28 -0600

    Update train.py to create spark session, load data, and proceed to train

--- src/train.py
+++ src/train.py
@@ -2,21 +2,30 @@ from pyspark.ml.classification import LogisticRegression
 from pyspark.ml import Pipeline, PipelineModel
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import when, col
-from preprocess import preprocess_data
+# from preprocess import preprocess_data
 
 def train_model(data_path, model_path):
-    
-    df, _ = df.randomSplit([0.8, 0.2], seed=42)
-    df, stages = preprocess_data(data_path)
+
+  spark = SparkSession.builder.appName("FraudDetection").config("spark.hadoop.fs.s3a.aws.credentials.provider", "com.amazonaws.auth.DefaultAWSCredentialsProviderChain").config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem").config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.11.1026").getOrCreate()
+
+  # load data
+  df = spark.read.parquet(data_path)
+
+  # split data
+
+  df, _ = df.randomSplit([0.8, 0.2], seed=42)
+  df, stages = preprocess_data(df)
 
     # add logistic regression to pipeline
-    lr = LogisticRegression(featuresCol='features', labelCol='is_fraud', weightCol='weight')
+  lr = LogisticRegression(featuresCol='features', labelCol='is_fraud', weightCol='weight')
 
-    stages.append(lr)
+  stages.append(lr)
 
-    # build pipeline
-    pipeline = Pipeline(stages=stages)
-    model = pipeline.fit(df)
+  # build pipeline
+  pipeline = Pipeline(stages=stages)
+  model = pipeline.fit(df)
 
-    # save model, will update location later
-    model.save('/content/drive/MyDrive/Documents/Data Career/fraud_classification/fraud_class_pyspark')
+  # save model, will update location later
+  model.write().overwrite().save(model_path)
+
+  return model
